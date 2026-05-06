244b8b9bd2175fae4fc0bef5cae48a4caabd9153
Fellan-91 <alexey.fellan@gmail.com>
2026-05-06 12:10:51 +0300

    Build admin assets for settings update

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '27c0bd861d8492593086');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '06000b8655ab7634c3a6');
