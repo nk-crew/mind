e9f23efe63c84a697adf1fdf06392bd7ded18bdd
Fellan-91 <alexey.fellan@gmail.com>
2026-05-06 13:28:16 +0300

    build: regenerate admin assets

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '06000b8655ab7634c3a6');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '32bd0da84f019800144d');
